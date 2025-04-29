61ee62c84d45b53b603a8f6bbf97e68b4f59f0cc
Kaelan Jantzen <20606816+Koolstir@users.noreply.github.com>
2025-04-28 17:30:04 -0700

    More iterations

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,21 @@ export default function Home() {
         <div className="border-2 rounded-lg p-3  m-1 text-center font-bold w-50">
           Welcome!
         </div>
-        <div className="pt-5">So far this is all I've been able to come up with!</div>
+        <div className="pt-5">
+          So far this is all I've been able to come up with!
+        </div>
+
+        <img
+          className="rounded-full w-96 h-96 border-8 hover:border-[#E3F1F8]"
+          src="/cat_roomba.jpg"
+          alt="Cat Roomba"
+        ></img>
+
+        <h2 className="w-132 text-2xl font-bold">
+          Hi! I'm Kaelan Jantzen, a 3rd computer scientist studying at UVIC.
+          This website has refences to all my personal projects I've developed
+          over time.
+        </h2>
       </div>
     </div>
   );
