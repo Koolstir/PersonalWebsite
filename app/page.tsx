29d712515eee8bf9dd509174afada4b62ae045cc
Kaelan Jantzen <20606816+Koolstir@users.noreply.github.com>
2025-05-01 16:15:07 -0700

    More work done on about

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/about/About";
 import Heading from "@/components/header/Heading";
 import Image from "next/image";
 
@@ -5,26 +6,28 @@ export default function Home() {
   return (
     <div>
       <Heading />
-      <div className="flex flex-col items-center gap- pt-5">
+      <section className="flex flex-col items-center pt-5">
         <div className="border-2 rounded-lg p-3  m-1 text-center font-bold w-50">
           Welcome!
         </div>
-        <div className="pt-5">
-          So far this is all I've been able to come up with!
+        <div className="mt-20">
+          <img
+            className="rounded-full w-96 h-96 border-8 hover:border-[#E3F1F8]"
+            src="/cat_roomba.jpg"
+            alt="Cat Roomba"
+          ></img>
         </div>
 
-        <img
-          className="rounded-full w-96 h-96 border-8 hover:border-[#E3F1F8]"
-          src="/cat_roomba.jpg"
-          alt="Cat Roomba"
-        ></img>
-
-        <h2 className="w-132 text-2xl font-bold">
-          Hi! I'm Kaelan Jantzen, a 3rd computer scientist studying at UVIC.
-          This website has refences to all my personal projects I've developed
-          over time.
-        </h2>
-      </div>
+        <p className="w-150 text-2xl font-bold pt-20 text-center text-gray-900">
+          Hi! I'm <span className="text-blue-500">Kaelan Jantzen</span>, a 3rd
+          year computer scientist studying at UVIC. This website has references
+          to my personal projects I've developed over time.
+        </p>
+      </section>
+      <section className="flex flex-col items-center pt-5">
+        <About/>
+        <span id="About"></span>
+      </section>
     </div>
   );
 }
---
+++ components/about/About.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <>
+      <div className="mt-20 mb-10 text-4xl font-semibold text-center">About</div>
+      <div className="mx-10 mb-10 text-lg font-mono w-150 text-center">Proficient in web development with expertise in NextJS and Postgres, having led the design and creation of an internal employee training portal. Experienced in automation using PowerShell for streamlined onboarding and offboarding processes. Skilled in website redesign, demonstrated by overhauling a 50-page official website, and adept in implementing CI/CD changes for Azure services to enhance development workflows.</div>
+
+
+    </>
+  );
+}
